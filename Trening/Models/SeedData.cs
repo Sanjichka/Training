671ac7b8896a2e285686c3a328abc861ed88aa54
Sanjichka <72527617+Sanjichka@users.noreply.github.com>
2021-08-10 15:10:13 +0200

    sd

--- Trening/Models/SeedData.cs
+++ Trening/Models/SeedData.cs
@@ -82,20 +82,173 @@ namespace Trening.Models
                 CreateUserRoles(serviceProvider).Wait();
                 if (context.User.Any() || context.Coach.Any() || context.Training.Any() || context.Discipline.Any()) { return; }
 
-                context.User.AddRange(new User
-                {
-                    Username = "Sanjichka",
-                    Embg = "1234567891011",
-                    FirstName = "Sanja",
-                    LastName = "Siljanoska",
-                    Mail = "sanjavasil@gmail.com"
-                });
+                context.User.AddRange(
+                    new User {
+                        Username = "Sanjs",
+                        Embg = "1234567891011",
+                        FirstName = "Sanja",
+                        LastName = "Siljanoska",
+                        gender="F",
+                        City="Skopje",
+                        ExerciseLevel="Advanced",
+                        Mail = "sanjavasil@gmail.com"
+                    },
+                    new User
+                    {
+                        Username = "V V",
+                        Embg = "1234567891012",
+                        FirstName = "V",
+                        LastName = "V",
+                        gender="M",
+                        City="Bitola",
+                        Mail = "vks@gmail.com"
+                    },
+                    new User
+                    {
+                        Username = "P SV",
+                        Embg = "1234567891013",
+                        FirstName = "Pufi",
+                        LastName = "SV",
+                        gender = "j",
+                        City = "Negotino",
+                        Mail = "pufisv@gmail.com"
+                    }
 
-                context.Coach.AddRange(new Coach { Username = "Emi", ExerciseRank = "Advanced", Awards = "None" });
+                    );
 
-                context.Discipline.AddRange(new Discipline {DisciplineName = "Fitness", Type = "Individual", Equipment = "Exercise Mat, Bands, Weights", Ground = "Anywhere" });
+                context.Coach.AddRange(
+                    new Coach { 
+                        Username = "Emi",
+                        ExerciseRank = "Advanced",
+                        BirthDate= DateTime.Parse("2000-01-17"),
+                        Mail="emi@gmail.com"
+                    },
+                    new Coach { 
+                        Username = "Slavica",
+                        ExerciseRank = "Intermediate",
+                        BirthDate= DateTime.Parse("1998-02-03"),
+                        Mail="slavica@gmail.com"
+                    },
+                    new Coach
+                    {
+                        Username = "Aleks",
+                        ExerciseRank = "Advanced",
+                        BirthDate = DateTime.Parse("1991-01-22"),
+                        Mail = "aleks@gmail.com"
+                    },
+                    new Coach
+                    {
+                        Username = "Anna",
+                        ExerciseRank = "Advanced",
+                        BirthDate = DateTime.Parse("2000-01-20"),
+                        Mail = "anna@gmail.com"
+                    }
+                    );
 
-                context.Training.AddRange(new Training {TrainingName = "Emi's Personal Fitness Training", Platform = "Zoom", CompanyCoache = "FitMK", Price = 12.99M, NumClMonth = 12 });
+                context.Discipline.AddRange(
+                    new Discipline {
+                        DisciplineName = "Fitness", 
+                        Type = "Individual", 
+                        Equipment = "Exercise Mat, Bands, Weights", 
+                        Ground = "Anywhere" 
+                    },
+                    new Discipline
+                    {
+                        DisciplineName = "Yoga",
+                        Type = "Individual",
+                        Equipment = "Exercise Mat",
+                        Ground = "Anywhere"
+                    },
+                    new Discipline
+                    {
+                        DisciplineName = "Dancing",
+                        Type = "Individual",
+                        Equipment = "Exercise Mat",
+                        Ground = "Anywhere"
+                    },
+                    new Discipline
+                    {
+                        DisciplineName = "Chess",
+                        Type = "Individual",
+                        Equipment = "Chess Board",
+                        Ground = "Anywhere"
+                    },
+                    new Discipline
+                    {
+                        DisciplineName = "Football Tricks",
+                        Type = "Individual",
+                        Equipment = "Football Ball",
+                        Ground = "Outside"
+                    },
+                    new Discipline
+                    {
+                        DisciplineName = "Pilates",
+                        Type = "Individual",
+                        Equipment = "Exercise Mat, Pilates Ball, Bands, Rings, Chair",
+                        Ground = "Anywhere"
+                    },
+                    new Discipline
+                    {
+                        DisciplineName = "Magic Tricks",
+                        Type = "Individual",
+                        Equipment = "Cards",
+                        Ground = "Anywhere"
+                    },
+                    new Discipline
+                    {
+                        DisciplineName = "Home Gym",
+                        Type = "Individual",
+                        Equipment = "Gym Equipment at Home",
+                        Ground = "Gym/Home Gym"
+                    }
+                    );
+
+                context.Training.AddRange(
+                    new Training {
+                        TrainingName = "Emi's Personal Fitness Training", 
+                        Platform = "Zoom", 
+                        CompanyCoache = "FitMK", 
+                        Price = 12.99M, 
+                        NumClMonth = 12,
+                        Discipline="Fitness"
+                    },
+                    new Training
+                    {
+                        TrainingName = "Yoga With Slavica",
+                        Platform = "Microsoft Teams",
+                        CompanyCoache = "FitMKz",
+                        Price = 10.99M,
+                        NumClMonth = 16,
+                        Discipline="Yoga"
+                    },
+                    new Training
+                    {
+                        TrainingName = "Home Gym With Aleks",
+                        Platform = "Skype",
+                        CompanyCoache = "FitMK",
+                        Price = 17.99M,
+                        NumClMonth = 18,
+                        Discipline = "Gym/Home Gym"
+                    },
+                    new Training
+                    {
+                        TrainingName = "Dance With Anna",
+                        Platform = "Zoom",
+                        CompanyCoache = "FitMKz",
+                        Price = 20.99M,
+                        NumClMonth = 13,
+                        Discipline = "Dancing"
+                    },
+                    new Training
+                    {
+                        TrainingName = "Pilates With Emi",
+                        Platform = "Zoom",
+                        CompanyCoache = "FitMK",
+                        Price = 16.99M,
+                        NumClMonth = 13,
+                        Discipline = "Pilates"
+                    }
+                    );
 
                 context.Enrollment.AddRange(new Enrollment { UserID = 1, TrainingID = 1, Owe = 0M});
 
